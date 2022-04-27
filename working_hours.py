796cf839b82d3ff2a74c85acb6b985de0c8b651c
oshiro <kaito.o.0628@gmail.com>
2022-04-27 15:37:47 +0900

    使用していないコードやコメントの削除、変数名の変更

--- working_hours.py
+++ working_hours.py
@@ -1,5 +1,4 @@
 # coding: UTF-8
-import json
 import datetime
 import calendar
 import re
@@ -11,68 +10,37 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-#エラーが起きたら下記「chromedriver_binary」「webdriver_manager」インストールしてください
-import chromedriver_binary
 from webdriver_manager.chrome import ChromeDriverManager
-import time
-
-# ********************　必読　********************
-
-# ■　■　■　■　■　■　■　前提条件 ■　■　■　■　■　■　■　
-
-# Python3がインストール済みであること
-# ブラウザ「Google Chrome」がインストール済みであること
-
-# コマンドラインで以下をインストール
-#             pip install selenium
-# brewをアップデート
-# 　　　　　　　brew tap homebrew/cask
-# chromedriverをインストール
-# 　　　　　　　brew install chromedriver
-
-# ■　■　■　■　■　■　■　手順 ■　■　■　■　■　■　■　
-
-# 1:user_idにKING OF TIME の ID を入力
-
-# 2:my_passにKING OF TIME の パスワード を入力
-
-# 3:range_startからrange_endまで各項目を入力（月初から月末の場合は変更なしでOK）
-
-# 4:ファイルを保存してターミナルを起動
-
-# 5:python working_hours_csv.py　入力
-
-# 6:自動的にchromeが起動される
-# ※macはこのスクリプトの初回起動時にシステム環境設定＞セキュリティより許可が必要
 
 # ********************個人設定********************
 
-
-# KING OF TIME の ID を入力
+# KING OF TIMEのIDを入力する
 user_id = ""
 
-# KING OF TIME の パスワード を入力
+# KING OF TIMEのパスワードを入力する
 my_pass = ""
 
-# 取得範囲 ※1日から取得する場合は0
+# 取得範囲 ※1日から取得する場合は0を指定する
 range_start = 0
-# 末日
+# 末日を指定する
 today = datetime.date.today()
 lastday = calendar.monthrange(today.year, today.month)
 range_end = lastday[1]
 
+# Apps ScriptのURLを入力する
+url = ""
+
 # ********************コード********************
 
-# KOTから値を取得
-def getText(path):
+# KING OF TIMEの勤務時間入力欄から値を取得する
+def getInputValue(path):
     return webdriver.find_element_by_xpath(path).text
 
-# 勤務時間を 00:00 の形式にする
-def getTime(time):
-    return re.sub("\D\D", "", time)
+# 取得した勤務時間の値を 編 00:00 から 00:00 の形式にする
+def changeInputValue(inputValue):
+    return re.sub("^\D*", "", inputValue)
 
 webdriver = webdriver.Chrome(ChromeDriverManager().install())
-#webdriver = webdriver.Chrome()
 webdriver.get("https://s2.kingtime.jp/admin/yC6jmVwSsiEOPtg6kcE7uf3XZpZIi32KF")
 # 画面表示まで待つ
 element = WebDriverWait(webdriver, 10).until(
@@ -88,47 +56,30 @@ element = WebDriverWait(webdriver, 10).until(
     expected_conditions.presence_of_element_located((By.XPATH, "/html/body/div/div[2]/div/div[5]/div[1]/table/tbody/tr"))
 )
 
-# 日付ごとの勤務時間を格納（日付、スケジュール、出勤、退勤、休憩開始、休憩終了）
+# 勤務時間を格納する（日付、スケジュール、出勤、退勤、休憩開始、休憩終了）
 working_hours_list = {}
 
 for i in range(range_start,range_end):
-    # 日付欄
     day_xpath = "/html/body/div/div[2]/div/div[5]/div[1]/table/tbody/tr[" + str(i+1) +"]/td[2]/p"
-    # スケジュール欄
     day_schedule_xpath = "/html/body/div/div[2]/div/div[5]/div[1]/table/tbody/tr[" + str(i+1) +"]/td[4]/p"
-    # 出勤欄
-    td6_xpath = "/html/body/div/div[2]/div/div[5]/div[1]/table/tbody/tr[" + str(i+1) + "]/td[6]/p"
-    # 退勤欄
-    td7_xpath = "/html/body/div/div[2]/div/div[5]/div[1]/table/tbody/tr[" + str(i+1) + "]/td[7]/p"
-    # 休憩開始欄
-    td8_xpath = "/html/body/div/div[2]/div/div[5]/div[1]/table/tbody/tr[" + str(i+1) + "]/td[8]/p"
-    # 休憩終了欄
-    td9_xpath = "/html/body/div/div[2]/div/div[5]/div[1]/table/tbody/tr[" + str(i+1) + "]/td[9]/p"
+    start_work_xpath = "/html/body/div/div[2]/div/div[5]/div[1]/table/tbody/tr[" + str(i+1) + "]/td[6]/p"
+    end_work_xpath = "/html/body/div/div[2]/div/div[5]/div[1]/table/tbody/tr[" + str(i+1) + "]/td[7]/p"
+    start_break_xpath = "/html/body/div/div[2]/div/div[5]/div[1]/table/tbody/tr[" + str(i+1) + "]/td[8]/p"
+    end_break_xpath = "/html/body/div/div[2]/div/div[5]/div[1]/table/tbody/tr[" + str(i+1) + "]/td[9]/p"
     
     try :
-        # 日付
-        day = getText(day_xpath)
-        # スケジュール
-        day_schedule = getText(day_schedule_xpath)
-        # 出勤時間
-        td6 = getText(td6_xpath)
-        # 退勤時間
-        td7 = getText(td7_xpath)
-        # 休憩開始時間
-        td8 = getText(td8_xpath)
-        # 休憩終了時間
-        td9 = getText(td9_xpath)
+        day = getInputValue(day_xpath)
+        day_schedule = getInputValue(day_schedule_xpath)
+        start_work = getInputValue(start_work_xpath)
+        end_work = getInputValue(end_work_xpath)
+        start_break = getInputValue(start_break_xpath)
+        end_break = getInputValue(end_break_xpath)
 
-        # 日付、スケジュール
         working_hours_list[i] = {"day" : day, "day_schedule" : day_schedule}
-        # 出勤時間
-        working_hours_list[i]["start_work"] = getTime(td6)
-        # 退勤時間
-        working_hours_list[i]["end_work"] = getTime(td7)
-        # 休憩開始時間
-        working_hours_list[i]["start_break"] = getTime(td8)
-        # 休憩終了時間
-        working_hours_list[i]["end_break"] = getTime(td9)
+        working_hours_list[i]["start_work"] = changeInputValue(start_work)
+        working_hours_list[i]["end_work"] = changeInputValue(end_work)
+        working_hours_list[i]["start_break"] = changeInputValue(start_break)
+        working_hours_list[i]["end_break"] = changeInputValue(end_break)
     except :
         print("勤務時間の取得でエラーが発生しました。")
         break
@@ -137,13 +88,12 @@ webdriver.close()
 webdriver.quit()
 
 # スプレッドシートへの書き込み
-url = ""
-
 for working_hour in working_hours_list.values():
     try:
         response = requests.post(url, data=working_hour)
     except:
-        print("スプレッドシートへの書き込みでエラーが発生しました。")
+        response = "スプレッドシートへの書き込みでエラーが発生しました。"
+        print(response)
         break
 
 if response.status_code == requests.codes.ok:
